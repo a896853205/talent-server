1f58822a9f2363e1ca09eba8fb2ed2d290fda872
13836005450 <597985642@qq.com>
2019-08-30 09:12:18 +0800

    新增跨域配置

--- cors-config.js
+++ cors-config.js
@@ -1,16 +1,5 @@
-export const option = {
-  origin: function (ctx) {
-    return "127.0.0.1"; // 允许来自所有域名请求
-},
-exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
-maxAge: 5,
-credentials: true,
-allowMethods: ['GET', 'POST', 'DELETE'],
-allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
-}
-
 export const cors = async (ctx, next) => {
-    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Origin', 'http://localhost:8080');
     
     await next();
 }
